26ef41b9824b5492dc8ea4d22e7fa660978ad88b
YananW <40300331+xfxcwynlc@users.noreply.github.com>
2021-05-31 00:02:36 -0400

    Logic explains
    adds comments about the logic of this Q.

--- Array_easy/plusone.cpp
+++ Array_easy/plusone.cpp
@@ -5,12 +5,12 @@ public:
         for(int i=digits.size()-1;i>=0;i--){
             if(digits.at(i)<9){
                 digits.at(i)+=1;
-                return digits;
+                return digits; //we will return it once a digit is not ==9 anymore.
             }else{
-                digits.at(i) = 0;
+                digits.at(i) = 0; //change current digit to 0, as it was 9 before.
             }
         }
-        //insert 1 at the begining
+        //insert 1 at the begining, this happens only if the nums are 9,9,..,9. 
         digits.insert(digits.begin(),1);
         return digits;
     }
