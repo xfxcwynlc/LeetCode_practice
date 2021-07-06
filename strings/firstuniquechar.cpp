7cd4049c9ad64fef205aa7806de7741d6e90aa19
YananW <40300331+xfxcwynlc@users.noreply.github.com>
2021-07-05 23:46:31 -0400

    Create firstuniquechar.cpp
    https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/881/

---
+++ strings/firstuniquechar.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char,int> dict;
+        for(int i=0;i<s.length();i++){
+            dict[s[i]]+=1;
+        }
+        
+        for(int i=0;i<s.length();i++){
+            if (dict[s[i]]==1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
